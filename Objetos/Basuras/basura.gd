937136193ba4c6106fc9f5494afbcfafa8859fe0
Sbtsu <138275214+Sbtsu@users.noreply.github.com>
2024-11-11 19:40:26 -0500

    nuevas animaciones, sprite de player y arreglos en grupos

--- Objetos/Basuras/basura.gd
+++ Objetos/Basuras/basura.gd
@@ -12,13 +12,10 @@ func _on_area_2d_mouse_exited() -> void:
 	on_basura = false
 	print("mouse salio de basura")
 
-func _input(event):
-	if event is InputEventKey and event.pressed:
-		if event.keycode == KEY_T:
-			is_clicked = true
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT and on_basura == true:
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT and on_basura == true:
 		is_clicked = true
-		print("You clicked on Sprite!")
+		print("Basura presionada")
 
 func when_basura_clicked():
 	if is_clicked == true:
@@ -30,17 +27,17 @@ func _physics_process(delta):
 	when_basura_clicked()
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
-	if area.is_in_group("aprovechable") and is_in_group("aprovechable"):
+	if area.is_in_group("caneca_aprovechable") and is_in_group("aprovechable"):
 		Global.puntos += 1
 		print("una basura entro en: aprovechable")
 		print(Global.puntos)
 		queue_free()
-	if area.is_in_group("organicos") and is_in_group("organicos"):
+	if area.is_in_group("caneca_organica") and is_in_group("organicos"):
 		Global.puntos += 1
 		print("una basura entro en: organicos")
 		print(Global.puntos)
 		queue_free()
-	if area.is_in_group("no aprovechable") and is_in_group("no aprovechable"):
+	if area.is_in_group("caneca_no_aprovechable") and is_in_group("no aprovechable"):
 		Global.puntos += 1
 		print("una basura entro en: no aprovcechable")
 		print("puntuacion actual: ", Global.puntos)
