dc8db3b047d722803a18b21f86e5f053f4522bf4
Sbtsu <138275214+Sbtsu@users.noreply.github.com>
2024-11-12 01:38:22 -0500

    hotfix

--- Objetos/spawn_basura.gd
+++ Objetos/spawn_basura.gd
@@ -8,6 +8,7 @@ var basura_organica_manzana = preload("res://Objetos/Basuras/basura_organica_man
 
 @export var basura_pos_x = 32
 @export var basura_pos_y = 384
+@export var basura_max = Global.puntos_max
 var random_index_basura = 0
 
 # Called when the node enters the scene tree for the first time.
@@ -16,7 +17,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _on_timer_timeout() -> void:
-	if Global.cantidad_basura < Global.puntos_max:
+	if Global.cantidad_basura < basura_max:
 		var random_index_basura = randi() % 3
 		print("index de basura actual es: ", random_index_basura)
 		if random_index_basura == 0:
